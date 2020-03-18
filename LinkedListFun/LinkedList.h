88658982d92403996e22d480f1053681204cb0bc
Gina Sprint <sprint@gonzaga.edu>
2020-03-17 19:23:10 -0700

    LinkedList

---
+++ LinkedListFun/LinkedList.h
@@ -0,0 +1,36 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <iostream>
+
+using namespace std;
+
+// a linked list of Nodes
+// each Node stores a single integer value
+// can be expanded to store multiple values
+// or templated to store any value type
+class LinkedList {
+	private:
+		// self referential
+	   struct Node {
+		  int value; // data value Node stores
+		  struct Node * next; // points to next Node in list
+	   }; 
+
+	   Node * head; // list head pointer
+
+	public:
+	   LinkedList(); // DVC
+	   ~LinkedList(); // destructor
+		  
+	   // common linked list operations
+	   void insertAtFront(int);
+	   void displayList();
+	   void appendNode(int); // insert at end
+	   void insertInOrder(int);
+	   void deleteAtFront();
+	   void deleteAtEnd();
+	   void deleteNode(int);
+};
+
+#endif
---
+++ LinkedListFun/Main.cpp
@@ -0,0 +1,50 @@
+#include "LinkedList.h"
+
+int main() {
+    LinkedList list; // start out empty list
+	
+	list.insertAtFront(12);
+	list.insertAtFront(5);
+	list.insertAtFront(3);
+	
+	list.displayList();
+	/*list.deleteNode(3); // delete first node
+	list.displayList();
+	list.deleteNode(5);
+	list.displayList();
+	list.deleteNode(12);
+	list.displayList();
+	list.deleteNode(4);
+	list.displayList();*/
+	
+	
+	/*list.deleteAtEnd();
+	list.displayList();
+	list.deleteAtEnd();
+	list.displayList();
+	list.deleteAtEnd();
+	list.displayList();
+	list.deleteAtEnd();
+	list.displayList();*/
+	
+	/*list.deleteAtFront();
+	list.displayList();
+	list.deleteAtFront();
+	list.displayList();
+	list.deleteAtFront();
+	list.displayList();
+	list.deleteAtFront();
+	list.displayList();
+	*/
+	
+	//list.appendNode(15);
+	/*list.displayList();
+	list.insertInOrder(7);
+	list.displayList();
+	list.insertInOrder(2);
+	list.displayList();
+	list.insertInOrder(15);
+	list.displayList();*/
+   
+   	return 0;
+}
